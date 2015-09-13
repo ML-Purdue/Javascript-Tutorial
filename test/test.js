3a87e13d68d0eb7c854af2714953ad72268823b4
Matthew Page <matthewpagecs@gmail.com>
2015-09-12 20:17:33 -0400

    Added testing file

---
+++ test/test.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+window.alert("Hello, World!"); // Creates a pop-up window that says "Hello World!"
+document.write("Hello, World!"); // Writes to the HTML document
+console.log("Hello, World!"); // Writes to the JavaScript Console
+
+var numStudents = 72; // Example of an int
+var minimalWage = 7.5; // Example of a double
+var myName = "Bill Gates"; // Example of a string
+var type = 'c'; // Actually a string, not a char. There is no primitive char type in JavaScript.
+var happy = true; // Example of a boolean variable
+
+document.write(5 + 2);
+document.write(5 - 2);
+document.write(5 * 2);
+document.write(5 / 2);
+document.write(5 % 2);
+document.write(5.0 / 2);
